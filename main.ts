4e32454db968226aeb10d731215a1ae65ee820ff
Joey Wunderlich <jwunderl@users.noreply.github.com>
2018-09-04 10:48:30 -0700

    build class

--- main.ts
+++ main.ts
@@ -1,35 +1,129 @@
 namespace procedural {
-    let parts: Image[][] = [];
-    // TODO: create example parts in separate 'sheets.ts' file
+    export class SpriteGenerator {
+        private parts: Image[][];
+        private previous: Image[];
+        private colors: number[];
+        // TODO: create example parts in separate 'sheets.ts' file
 
-    function generate(input: Image): Image {
-        let precedenceMap = input.clone();
-        precedenceMap.fill(0);
-        let output = precedenceMap.clone();
+        // Maybe give an enum with a handful of default parts sheets,
+        // and optionally accept one of those?
+        constructor() {
+            this.parts = [];
+            this.previous = undefined;
+            this.colors = [];
+            for (let i = 0; i <= 0xF; i++) {
+                this.colors.push(i);
+            }
+        }
 
-        for (let i = 0; i < input.width; i++) {
-            for (let j = 0; j < input.height; j++) {
-                let color = input.getPixel(i, j)
-                if (color && parts[color]) {
-                    copyOver(i,
+        generate(input: Image, usePrevious?: boolean): Image {
+            let precedenceMap = input.clone();
+            precedenceMap.fill(0);
+            let output = precedenceMap.clone();
+
+            let chosen: Image[] = [];
+            if (usePrevious && this.previous) {
+                for (let color = 0; color < chosen.length; color++) {
+                    if (this.parts[color]) {
+                        chosen[color] = Math.pickRandom(this.parts[color]);
+                    }
+                }
+            } else {
+                chosen = this.previous;
+            }
+
+            this.previous = chosen;
+
+            for (let i = 0; i < input.width; i++) {
+                for (let j = 0; j < input.height; j++) {
+                    let color = input.getPixel(i, j)
+                    if (color && chosen[color]) {
+                        copyOver(i,
                             j,
                             color,
                             output,
-                            Math.pickRandom(parts[color]),
+                            chosen[color],
                             precedenceMap
-                            );
+                        );
+                    }
                 }
             }
+            return output;
         }
-        return output;
+
+        colorSwap(input: Image, colors?: number[]): Image {
+            if (!colors || colors.length != 16) {
+                // shuffle 1-14; leave black and clear as is
+                colors = [];
+                for (let i = 1; i < 0xf; ++i) {
+                    colors.push(i);
+                }
+                shuffle(colors);
+                colors.insertAt(0, 0x0);
+                colors.push(0xf);
+            }
+            let outImage = input.clone();
+            for (let i = 0; i < outImage.width; i++) {
+                for (let j = 0; j < outImage.height; j++) {
+                    outImage.setPixel(i, j, this.colors[outImage.getPixel(i, j)]);
+                }
+            }
+            return outImage;
+        }
+
+        setColors(colors?: number[]) {
+            if (!colors || colors.length != 16) {
+                // shuffle 1-14; leave black and clear as is
+                this.colors = [];
+                for (let i = 1; i < 0xf; ++i) {
+                    this.colors.push(i);
+                }
+                shuffle(this.colors);
+                this.colors.insertAt(0, 0x0);
+                this.colors.push(0xf);
+            } else {
+                this.colors = colors;
+            }
+        }
+
+        saveImage(input: Image): void {
+            let output = "img`\n";
+            for (let row = 0; row < input.height; row++) {
+                for (let col = 0; col < input.width; col++) {
+                    let val = input.getPixel(col, row);
+                    output += ((val < 0xa) ?
+                        val + ""
+                        : "ABCDEF".charAt(val % 10))
+                        + " ";
+                }
+                output += "\n";
+            }
+            console.log(output + "`");
+        }
+
+        addPart(color: number, part: Image): void {
+            if (invalidColor(color) || !part) return;
+
+            if (!this.parts[color]) this.parts[color] = [part];
+            else this.parts[color].push(part);
+        }
+
+        setParts(color: number, parts: Image[]): void {
+            if (invalidColor(color) || !parts) return;
+            for (let part of parts) {
+                if (!part) return;
+            }
+
+            this.parts[color] = parts;
+        }
     }
 
     function copyOver(x: number,
-                        y: number,
-                        precedence: number,
-                        target: Image,
-                        toCopy: Image,
-                        precedenceMap: Image) {
+        y: number,
+        precedence: number,
+        target: Image,
+        toCopy: Image,
+        precedenceMap: Image) {
         x -= toCopy.width / 2
         y -= toCopy.height / 2;
         for (let i = 0; i < toCopy.width; i++) {
@@ -46,26 +140,6 @@ namespace procedural {
         }
     }
 
-    function colorSwap(input: Image, colors?: number[]): Image {
-        if (!colors || colors.length != 16) {
-            // shuffle 1-14; leave black and clear as is
-            colors = [];
-            for (let i = 1; i < 0xf; ++i) {
-                colors.push(i);
-            }
-            shuffle(colors);
-            colors.insertAt(0, 0x0);
-            colors.push(0xf);
-        }
-        let outImage = input.clone();
-        for (let i = 0; i < outImage.width; i++) {
-            for (let j = 0; j < outImage.height; j++) {
-                outImage.setPixel(i, j, colors[outImage.getPixel(i, j)]);
-            }
-        }
-        return outImage;
-    }
-
     function shuffle(input: number[]): void {
         for (let i = 0; i < input.length; ++i) {
             let ind = Math.randomRange(0, input.length - 1);
@@ -75,18 +149,7 @@ namespace procedural {
         }
     }
 
-    function saveImage(input: Image): void {
-        let output = "img`\n";
-        for (let row = 0; row < input.height; row++) {
-            for (let col = 0; col < input.width; col++) {
-                let val = input.getPixel(col, row);
-                output += ((val < 0xa) ?
-                            val + ""
-                            : "ABCDEF".charAt(val % 10))
-                            + " ";
-            }
-            output += "\n";
-        }
-        console.log(output + "`");
+    function invalidColor(color: number): boolean {
+        return color < 0 || color > 0xf;
     }
 }
